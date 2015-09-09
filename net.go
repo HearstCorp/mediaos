96d71cb7d029b0aad11994076f1851fc06f6a4cb
rravenel <rravenel@hearst.com>
2015-09-09 15:28:01 -0700

    custom api endpints

--- net.go
+++ net.go
@@ -1,13 +1,58 @@
 package mediaos
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
-var urlTemplate = "https://{publication}.hearst.io/api/v1/{endpoint}"
+const (
+	HTTP  = "http"
+	HTTPS = "https"
+
+	MEDIAOS_HTTP_SECURE = "MEDIAOS_HTTP_SECURE"
+
+	MEDIAOS_COSMO_DOMAIN     = "MEDIAOS_COSMO_DOMAIN"
+	MEDIAOS_ELLE_DOMAIN      = "MEDIAOS_ELLE_DOMAIN"
+	MEDIAOS_SEVENTEEN_DOMAIN = "MEDIAOS_SEVENTEEN_DOMAIN"
+
+	MEDIAOS_COSMO_PORT     = "MEDIAOS_COSMO_PORT"
+	MEDIAOS_ELLE_PORT      = "MEDIAOS_ELLE_PORT"
+	MEDIAOS_SEVENTEEN_PORT = "MEDIAOS_SEVENTEEN_PORT"
+)
+
+var defaultDomain = "hearst.io"
+var urlTemplate = "{protocol}://{domainPort}/api/v1/{endpoint}"
+var protocol = ""
+
+var domains map[Publication]string
+
+func init() {
+	secure := os.Getenv(MEDIAOS_HTTP_SECURE)
+	if "" == secure || "true" == secure {
+		protocol = HTTPS
+	} else {
+		protocol = HTTP
+	}
+
+	domains = make(map[Publication]string)
+
+	populateDomains(Cosmo, MEDIAOS_COSMO_DOMAIN, MEDIAOS_COSMO_PORT)
+	populateDomains(Elle, MEDIAOS_ELLE_DOMAIN, MEDIAOS_ELLE_PORT)
+	populateDomains(Seventeen, MEDIAOS_SEVENTEEN_DOMAIN, MEDIAOS_SEVENTEEN_PORT)
+}
+
+func populateDomains(pub Publication, domainVar, portVar string) {
+	d := os.Getenv(domainVar)
+	p := os.Getenv(portVar)
+
+	if "" != d && "" != p {
+		domains[pub] = fmt.Sprintf("%s:%s", d, p)
+	}
+}
 
 func doAPICall(endpoint Endpoint, req Request) (result []byte, err error) {
 	uri := prepareAPIUri(endpoint, req)
@@ -17,7 +62,13 @@ func doAPICall(endpoint Endpoint, req Request) (result []byte, err error) {
 }
 
 func prepareAPIUri(endpoint Endpoint, req Request) (uri string) {
-	uri = strings.Replace(urlTemplate, "{publication}", string(req.publication), 1)
+	dp := domains[req.publication]
+	if "" == dp {
+		dp = fmt.Sprintf("%s.%s", req.publication, defaultDomain)
+	}
+
+	uri = strings.Replace(urlTemplate, "{protocol}", protocol, 1)
+	uri = strings.Replace(uri, "{domainPort}", dp, 1)
 	uri = strings.Replace(uri, "{endpoint}", string(endpoint), 1)
 
 	params := prepareParams(req.key, req)
