6eb92dd93736d8b9b99092e69339f199843aedc1
Amelia Catalano <acatalano@hearst.com>
2015-12-07 14:48:25 -0800

    adding separate index_title field

--- model.go
+++ model.go
@@ -49,6 +49,7 @@ type Request struct {
 	ID              int // articles
 	GroupID         int // galleries, for some reason
 	Title           string
+	IndexTitle      string
 	Slug            string
 	ContentTypeID   []int
 	SectionID       []int
@@ -93,7 +94,8 @@ type Content struct {
 	ContentID  int        `json:"content_id"`
 	GroupID    int        `json:"group_id"`
 	Type       ResType    `json:"resource_type"`
-	Title      string     `json:"index_title"`
+	Title      string     `json:"title"`
+	IndexTitle string     `json:"index_title"`
 	Images     []Image    `json:"images"`
 	URL        string     `json:"url"`
 	AdCategory AdCategory `json:"ad_category"`
